1aeb1bbae1f43b32296482f0c7eb6db4d04e0f9f
w1419837887 <1419837887@qq.com>
2025-06-11 11:10:17 +0800

    删除src

--- src/execution/check_condition.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "common/common.h"
-#include <system/sm.h>
-bool check_condition(const RmRecord& record, TabMeta& tab_, const Condition& cond);
-
-bool check_condition(const RmRecord& record, TabMeta& tab_, const std::vector<Condition>& conds);
