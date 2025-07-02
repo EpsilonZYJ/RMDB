df1722c1be6dc6fca17a049e0f598ea3f13c2d79
w1419837887 <1419837887@qq.com>
2025-07-02 16:44:18 +0800

    修改了unit_test

--- src/unit_test.cpp
+++ src/unit_test.cpp
@@ -29,13 +29,13 @@ See the Mulan PSL v2 for more details. */
 #include "gtest/gtest.h"
 #include "replacer/lru_replacer.h"
 #include "storage/disk_manager.h"
-
+#include "gtest/gtest.h"
 #define private public
 #include "record/rm.h"
 #include "storage/buffer_pool_manager.h"
 
 #undef private
-#include "gtest/gtest.h"
+
 const std::string TEST_DB_NAME = "BufferPoolManagerTest_db";  // 以数据库名作为根目录
 const std::string TEST_FILE_NAME = "basic";                   // 测试文件的名字
 const std::string TEST_FILE_NAME_CCUR = "concurrency";        // 测试文件的名字
