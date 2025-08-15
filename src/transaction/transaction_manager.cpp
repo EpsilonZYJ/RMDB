12e16b7146921b7d1ae3a21a4f746c946aa3acc0
w1419837887 <1419837887@qq.com>
2025-08-15 11:54:08 +0800

    继续解除

--- src/transaction/transaction_manager.cpp
+++ src/transaction/transaction_manager.cpp
@@ -69,7 +69,7 @@ void TransactionManager::commit(Transaction* txn, LogManager* log_manager) {
     }
 
     CommitLogRecord log_record(txn->get_transaction_id());
-    // log_manager->add_log_to_buffer(&log_record);
+    log_manager->add_log_to_buffer(&log_record);
 
     // txn->set_state(TransactionState::COMMITTED);
 }
